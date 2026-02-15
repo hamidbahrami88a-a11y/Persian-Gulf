933c89d0e390d57923d2daa8e01766774607b900
Hamid Bahrami <hamidbahrami88a@gmail.com>
2026-02-15 16:19:46 +0330

    Fix ALLOWED_HOSTS for Render

--- digikala/settings.py
+++ digikala/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-w^3xhcem744=hyg4-zxwf5&e-o)qox(xg4-3+i)!!q9ak974@8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://persian-gulf.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
