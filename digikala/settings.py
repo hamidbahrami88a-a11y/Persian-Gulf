fe8db184521dd8d82a06ab1fc98faaff9c0de2f3
Hamid Bahrami <hamidbahrami88a@gmail.com>
2026-02-21 12:57:12 +0330

    Add Cloudinary for media storage and fix product template

--- digikala/settings.py
+++ digikala/settings.py
@@ -134,9 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = '/var/data/media' 
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
